54993678c7036ddb12d7043c9be0d38fc0ee5c34
wgohintuit <wenhan_goh@intuit.com>
2015-12-07 13:34:34 -0800

    commit

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,14 @@
-require([""], function(){
+require(["UserModel"], function(UserModel){
+    UserModel();
+    $('.target').html('jQuery is working <br>');
+    if (_.contains([1, 2, 3], 3)) {
 
+        $('.target').append('Underscore is working <br>');
+
+    }
+    var model = new Backbone.Model({isWorking: 'working'});
+
+    $('.target').append('Backone is ' + model.get
+
+        ('isWorking'));
 });
---
+++ dev/js/userModel.js
@@ -0,0 +1,6 @@
+define([], function(){
+
+  // Define a User Model with defaults and an initializer
+  var UserModel = function(){console.log("hello2");};
+  return UserModel;
+});
